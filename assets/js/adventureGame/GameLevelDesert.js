95fddaa16734f903ee6fdfa33aa07b94348e372e
katherinelaw1 <katherinel3342@gmail.com>
2025-03-05 23:06:28 -0800

    dd

--- assets/js/adventureGame/GameLevelDesert.js
+++ assets/js/adventureGame/GameLevelDesert.js
@@ -6,6 +6,7 @@ import Quiz from './Quiz.js';
 import GameControl from './GameControl.js';
 import GameLevelStarWars from './GameLevelStarWars.js';
 
+<<<<<<< HEAD
 class GameLevelDesert {
   constructor(gameEnv) {
     // Values dependent on this.gameEnv.create()
@@ -37,6 +38,8 @@ class GameLevelDesert {
       updateBackground(image_data_desert.src);
     }
 
+=======
+>>>>>>> 80fd0d6 (dd)
     // Player data for Chillguy
     const sprite_src_chillguy = path + "/images/gamify/Ali_A.jpeg"; // be sure to include the path
     const CHILLGUY_SCALE_FACTOR = 5;
@@ -239,7 +242,10 @@ class GameLevelDesert {
       { class: Npc, data: sprite_data_tux },
     ];
     
-  }
 
+<<<<<<< HEAD
 }
+=======
+
+>>>>>>> 80fd0d6 (dd)
 export default GameLevelDesert
